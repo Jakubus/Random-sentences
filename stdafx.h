42cad443989c7c1888be29ad3fb3afc5dceac484
Jakubus <jakubussimus@gmail.com>
2014-08-03 16:38:17 +0200

    Cleaning
    Deleted a few not used libraries

--- stdafx.h
+++ stdafx.h
@@ -7,20 +7,13 @@
 
 #include "targetver.h"
 #include <string>
-#include <stdio.h>
 #include <tchar.h>
 #include <random>
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
 #include <fstream>
 #include <iomanip>
-#include <cmath>
-#include <complex>
 #include <list>
 #include <set>
 #include "word_extended.h"
 #include "markov_dict.h"
-
-
-// TODO: W tym miejscu odwo³aj siê do dodatkowych nag³ówków wymaganych przez program
